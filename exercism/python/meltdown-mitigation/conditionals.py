05cd886f159a732783a200b07cdef1647521ce9e
deniskibichiy <deniskibichiy@outlook.com>
2026-07-15 21:15:20 +0300

    Implement Exercism meltdown-mitigation solution

--- exercism/python/meltdown-mitigation/conditionals.py
+++ exercism/python/meltdown-mitigation/conditionals.py
@@ -19,7 +19,8 @@ def is_criticality_balanced(temperature, neutrons_emitted):
 
     """
 
-    pass
+    return temperature < 800 and neutrons_emitted > 500 and (temperature * neutrons_emitted) < 500000
+        
 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
@@ -44,9 +45,15 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
         (generated power/ theoretical max power)*100
         where generated power = voltage * current
     """
-
-    pass
-
+    generated_power = voltage * current
+    efficiency = (generated_power/ theoretical_max_power) * 100
+    if efficiency >= 80:
+        return "green"
+    if 80 > efficiency >= 60:
+        return "orange"
+    if 30 <= efficiency  < 60:
+        return "red"
+    return "black"
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
     """Assess and return status code for the reactor.
@@ -64,5 +71,9 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
         2. 'NORMAL' -> `temperature * neutrons per second` +/- 10% of `threshold`
         3. 'DANGER' -> `temperature * neutrons per second` is not in the above-stated ranges
     """
-
-    pass
+    current_value = temperature * neutrons_produced_per_second
+    if current_value < 0.9 * threshold:
+        return "LOW"
+    if 0.9 * threshold <= current_value <= 1.1 * threshold:
+        return "NORMAL"
+    return "DANGER"
