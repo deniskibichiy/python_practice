a44309627f56d3ed08aab70649eae3ed3f4820c4
deniskibichiy <deniskibichiy@outlook.com>
2026-07-19 23:56:01 +0300

    Load baseball CSV dataset with NumPy

--- datacamp/introduction_to_python/04_numpy/02_subsetting_array.py
+++ datacamp/introduction_to_python/04_numpy/02_subsetting_array.py
@@ -1,5 +1,7 @@
 import numpy as np
-data = np.load("../../../datasets/baseball.csv")
+data = np.genfromtxt("datasets/baseball.csv",
+                     delimiter = ",",
+                     skip_header = 1)
 print(data.shape)
 np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
 np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.71])
