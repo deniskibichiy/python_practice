f4202baede6ac02c334b6948c242393dbbe78dee
deniskibichiy <deniskibichiy@outlook.com>
2026-07-20 22:36:55 +0300

    add and implement triangle check exercises to practices booleans

---
+++ exercism/python/triangle/triangle.py
@@ -0,0 +1,12 @@
+def test_validity(sides):
+    return sides[0] >0 and sides[1] > 0 and sides[2] > 0 and sides[0] + sides[1] > sides[2] and sides[1] + sides[2] > sides[0] and sides[0] + sides[2] > sides[1]
+
+def equilateral(sides):
+    return test_validity(sides) and sides[0] == sides[1] == sides[2]
+    
+
+def isosceles(sides):
+    return test_validity(sides) and (sides[0] == sides[1] == sides [2] or sides[0] == sides [1] or sides[1] == sides [2] or sides[0] == sides[2])
+
+def scalene(sides):
+    return test_validity(sides) and (sides[0] != sides[1] and sides[1] != sides[2] and sides[2] != sides[0])
