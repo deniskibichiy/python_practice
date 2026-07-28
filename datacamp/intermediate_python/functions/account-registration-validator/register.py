e98e055142372c80c9f3977cdc3688b7b20cff0c
deniskibichiy <deniskibichiy@outlook.com>
2026-07-28 23:27:50 +0300

    implement main function to validate users

--- datacamp/intermediate_python/functions/account-registration-validator/register.py
+++ datacamp/intermediate_python/functions/account-registration-validator/register.py
@@ -1,7 +1,30 @@
 from validators import (
-    validate_email,
     validate_name,
+    validate_email,
     validate_password,
 )
 
 
+def validate_user(name, email, password):
+    if not validate_name(name):
+        raise ValueError("Invalid name.")
+
+    if not validate_email(email):
+        raise ValueError("Invalid email.")
+
+    if not validate_password(password):
+        raise ValueError("Invalid password.")
+
+    return True
+
+
+try:
+    validate_user(
+        "Denis",
+        "denismutai5@gmail.com",
+        "Denis#4932"
+    )
+    print("Registration successful!")
+
+except ValueError as error:
+    print(error)
